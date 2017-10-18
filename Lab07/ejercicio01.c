6323cd2442c52da980522b972cc558b297d129ce
electrototo <cristobal@liendo.net>
2017-10-18 08:54:02 -0500

    modificar ejercicio

--- Lab07/ejercicio01.c
+++ Lab07/ejercicio01.c
@@ -8,6 +8,8 @@
 #include <time.h>
 #include <stdlib.h>
 
+int get_pos();
+
 int main() {
     int arr[20];
     srand(time(0));
@@ -16,7 +18,7 @@ int main() {
         arr[i] = 0;
 
     for (int i = 0; i < 20; i++)
-        printf("Valor de la posicion %d: %d\n", i, arr[i]);
+        printf("Valor de la posicion %d: %d\n", i + 1, arr[i]);
 
     printf("\n");
 
@@ -24,9 +26,38 @@ int main() {
         arr[i] = (rand() % 10) + 1;
 
     for (int i = 0; i < 20; i++)
-        printf("Valor de la posicion %d: %d\n", i, arr[i]);
+        printf("Valor de la posicion %d: %d\n", i + 1, arr[i]);
 
     printf("\n");
 
+    int pos, val;
+    pos = get_pos();
+
+    printf("Que valor quieres asignarle? ");
+    scanf("%d", &val);
+
+    arr[pos] = val;
+
+    pos = get_pos();
+
+    printf("Que valor quieres asignarle? ");
+    scanf("%d", &val);
+
+    arr[pos] = val;
+
+    for (int i = 0; i < 20; i++)
+        printf("Valor de la posicion %d: %d\n", i + 1, arr[i]);
+
     return 0;
+}
+
+int get_pos() {
+    int pos;
+    
+    do {
+        printf("Que posicion quieres cambiar? ");
+        scanf("%d", &pos);
+    } while(pos < 1 || pos > 20);
+
+    return pos - 1;
 }
