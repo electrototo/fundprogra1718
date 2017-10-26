72b5b164f4c45da0766f4820ce20feaf9f75b354
electrototo <cristobal@liendo.net>
2017-10-26 07:20:04 -0500

    Compact mastermind

--- Lab08/mastermindVersions/mastermind.c
+++ Lab08/mastermindVersions/mastermind.c
@@ -9,98 +9,6 @@
 #include <time.h>
 #include <string.h>
 
-char ascii_dado();
-int menu();
-void instrucciones();
-
-int get_input(char *);
-
-void generate_answer(char *, int *);
-
-int check_in(char *buf, char letter);
-
-int intersection(char *a, char *b, char *dest);
-
-int main() {
-    srand(time(0));
-
-    char resultado[5], resultado_copy[5], intento[5];
-    int cuenta[6], cuenta_copy[6];
-
-    // limpia cuenta
-    for (int i = 0; i < 6; i++)
-        cuenta[i] = 0;
-
-    generate_answer(resultado, cuenta);
-
-    int intentos = 0, won = 0;
-
-    // opcion escogida
-    int op;
-
-    // resultado actual
-    int res;
-
-    // caracteres que se van a imprimir
-    char hint[5];
-    int k = 0, index;
-
-    do {
-        op = menu();
-
-        if (op == 2)
-            instrucciones();
-
-    } while (op != 1 && op != 3);
-
-    if (op == 3)
-        return 0;
-
-    else {
-        while (intentos < 10 && !won) {
-            printf("\nIngresa intento: ");
-
-            if(get_input(intento) == 4) {
-                memset(hint, '\0', 5);
-
-                for (int j = 0; j < 6; j++)
-                    cuenta_copy[j] = cuenta[j];
-
-                strcpy(resultado_copy, resultado);
-
-                k = intersection(resultado_copy, intento, hint);
-
-                for (int i = 0; i < 4; i++) {
-                    index = intento[i] - 'a';
-
-                    res = check_in(resultado_copy, intento[i]);
-
-                    // obtiene los que estan pero en posiciones diferentes
-                    if (res == -1 && cuenta_copy[index] > 0) {
-                        hint[k++] = '#';
-                        cuenta_copy[index]--;
-                    }
-                }
-
-                printf("Hint: %s\n", hint);
-
-                if (strcmp(intento, resultado) == 0)
-                    won = 1;
-
-                intentos++;
-            }
-
-            else
-                printf("\nPor favor ingresa una combinacion de cuatro letras entre la a y la f.\n");
-        }
-
-        if (won)
-            printf("\nFelicidades, ganaste\n");
-        else
-            printf("\nLo lamento, perdiste\n\nEl resultado era: %s\n", resultado);
-    }
-}
-
 char ascii_dado() {
     return 'a' + (rand() % 6);
 }
@@ -191,4 +99,84 @@ int intersection(char *a, char *b, char *dest) {
     }
 
     return j;
+}
+
+int main() {
+    srand(time(0));
+
+    char resultado[5], resultado_copy[5], intento[5];
+    int cuenta[6], cuenta_copy[6];
+
+    // limpia cuenta
+    for (int i = 0; i < 6; i++)
+        cuenta[i] = 0;
+
+    generate_answer(resultado, cuenta);
+
+    int intentos = 0, won = 0;
+
+    // opcion escogida
+    int op;
+
+    // resultado actual
+    int res;
+
+    // caracteres que se van a imprimir
+    char hint[5];
+    int k = 0, index;
+
+    do {
+        op = menu();
+
+        if (op == 2)
+            instrucciones();
+
+    } while (op != 1 && op != 3);
+
+    if (op == 3)
+        return 0;
+
+    else {
+        while (intentos < 10 && !won) {
+            printf("\nIngresa intento: ");
+
+            if(get_input(intento) == 4) {
+                memset(hint, '\0', 5);
+
+                for (int j = 0; j < 6; j++)
+                    cuenta_copy[j] = cuenta[j];
+
+                strcpy(resultado_copy, resultado);
+
+                k = intersection(resultado_copy, intento, hint);
+
+                for (int i = 0; i < 4; i++) {
+                    index = intento[i] - 'a';
+
+                    res = check_in(resultado_copy, intento[i]);
+
+                    // obtiene los que estan pero en posiciones diferentes
+                    if (res == -1 && cuenta_copy[index] > 0) {
+                        hint[k++] = '#';
+                        cuenta_copy[index]--;
+                    }
+                }
+
+                printf("Hint: %s\n", hint);
+
+                if (strcmp(intento, resultado) == 0)
+                    won = 1;
+
+                intentos++;
+            }
+
+            else
+                printf("\nPor favor ingresa una combinacion de cuatro letras entre la a y la f.\n");
+        }
+
+        if (won)
+            printf("\nFelicidades, ganaste\n");
+        else
+            printf("\nLo lamento, perdiste\n\nEl resultado era: %s\n", resultado);
+    }
 }
---
+++ Lab08/mastermindVersions/mastermind2.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+char ascii_dado() {
+    return 'a' + (rand() % 6);
+}
+
+int get_input(char *arr) {
+    char c;
+
+    int i = 0;
+    while ((c = getchar()) != EOF && c != '\n' && i++ < 4 && c >= 'a' && c <= 'f')
+        *arr++ = c;
+
+    *arr = '\0';
+
+    if (c != '\n' && c != EOF)
+        while ((c = getchar()) != EOF && c != '\n');
+
+    return i;
+}
+
+void generate_answer(char *buf) {
+    for (int i = 0; i < 4; i++)
+        *buf++ = ascii_dado();
+
+    *buf = '\0';
+}
+
+int intersection(char *a, char *b, char *dest, char letter) {
+    int k = 0;
+
+    for (int i = 0; i < 4; i++)
+        if (a[i] == b[i] && a[i] != 'x') {
+            a[i] = 'x';
+            b[i] = 'x';
+
+            dest[k++] = letter;
+        }
+
+    return k;
+}
+
+void insertion_sort(char *a, int length) {
+    int key, k, tmp;
+    for (int i = 1; i < length; i++) {
+        key = a[i];
+        k = i - 1;
+        while (key < a[k] && k >= 0) {
+            tmp = a[k + 1];
+            a[k + 1] = a[k];
+            a[k] = tmp;
+
+            k--;
+        }
+    }
+}
+
+int merge(char *a, char *b, int length) {
+    int equal = 0, j = 0, k = 0;
+
+    while (k < length && j < length){
+        if (a[j] > b[k])
+            k++;
+        else if (a[j] < b[k])
+            j++;
+        else if (a[j] != 'x' && b[k] != 'x') {
+            equal++;
+            k++;
+        }
+        else
+            k++;
+    }
+
+    return equal;
+}
+
+int main() {
+    srand(time(0));
+
+    char respuesta[2][5], user[5], hint[5];
+
+    generate_answer(respuesta[0]);
+
+    printf("Respuesta: %s\n", respuesta[0]);
+
+    int won = 0, k;
+    for (int times = 0; times < 10 && !won; times++){
+        k = 0;
+
+        memset(hint, '\0', 5);
+        strcpy(respuesta[1], respuesta[0]);
+
+        printf("Ingresa respuesta: ");
+        get_input(user);
+
+        k = intersection(respuesta[1], user, hint, '!');
+
+        if (k == 4)
+            won = 1;
+
+        insertion_sort(respuesta[1], 4);
+        insertion_sort(user, 4);
+
+        for (int j = 0; j < merge(respuesta[1], user, 4) && k < 4; j++, k++)
+            hint[k] = '#';
+
+        printf("Hint: %s\n\n", hint);
+    }
+
+    if (!won)
+        printf("Perdiste. La respuesta era: %s\n", respuesta[0]);
+    else
+        printf("Ganaste!\n");
+}
