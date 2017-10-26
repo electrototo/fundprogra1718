0f86cf1a8cd5a6671765363386f1b9f3066fc7a8
electrototo <cristobal@liendo.net>
2017-10-26 07:54:07 -0500

    Add curses to mastermind

---
+++ Lab08/mastermindVersions/mastercurses.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <ncurses.h>
+
+char ascii_dado() {
+    return 'a' + (rand() % 6);
+}
+
+int get_input(char *arr) {
+    char c;
+
+    int i = 0;
+    while ((c = getch()) != EOF && c != '\n' && i++ < 4 && c >= 'a' && c <= 'f')
+        *arr++ = c;
+
+    *arr = '\0';
+
+    if (c != '\n' && c != EOF)
+        while ((c = getch()) != EOF && c != '\n');
+
+    return i;
+}
+
+void generate_answer(char *buf) {
+    for (int i = 0; i < 4; i++)
+        *buf++ = ascii_dado();
+
+    *buf = '\0';
+}
+
+int intersection(char *a, char *b, char *dest, char letter) {
+    int k = 0;
+
+    for (int i = 0; i < 4; i++)
+        if (a[i] == b[i] && a[i] != 'x') {
+            a[i] = 'x';
+            b[i] = 'x';
+
+            dest[k++] = letter;
+        }
+
+    return k;
+}
+
+void insertion_sort(char *a, int length) {
+    int key, k, tmp;
+    for (int i = 1; i < length; i++) {
+        key = a[i];
+        k = i - 1;
+        while (key < a[k] && k >= 0) {
+            tmp = a[k + 1];
+            a[k + 1] = a[k];
+            a[k] = tmp;
+
+            k--;
+        }
+    }
+}
+
+int merge(char *a, char *b, int length) {
+    int equal = 0, j = 0, k = 0;
+
+    while (k < length && j < length){
+        if (a[j] > b[k])
+            k++;
+        else if (a[j] < b[k])
+            j++;
+        else if (a[j] != 'x' && b[k] != 'x') {
+            equal++;
+            k++;
+        }
+        else
+            k++;
+    }
+
+    return equal;
+}
+
+void splash_screen() {
+    int row, col;
+    getmaxyx(stdscr, row, col);
+
+    char msg[] = "Mastermind";
+    char msg2[] = "Programado por cristobal liendo";
+
+    mvprintw(row/2, (col - strlen(msg))/2, msg);
+    mvprintw(row/2 + 1, (col - strlen(msg2))/2, msg2);
+
+    mvprintw(row - 1, 0, "Presiona cualquier tecla para iniciar...");
+
+    getch();
+}
+
+int main() {
+    initscr();
+    nocbreak();
+
+    splash_screen();
+    refresh();
+
+    clear();
+
+    srand(time(0));
+
+    char respuesta[2][5], user[5], hint[5];
+
+    generate_answer(respuesta[0]);
+
+    printw("Respuesta: %s\n", respuesta[0]);
+    refresh();
+
+    int won = 0, k;
+    for (int times = 0; times < 10 && !won; times++){
+        k = 0;
+
+        memset(hint, '\0', 5);
+        strcpy(respuesta[1], respuesta[0]);
+
+        printw("Ingresa respuesta: ");
+        get_input(user);
+
+        k = intersection(respuesta[1], user, hint, '!');
+
+        if (k == 4)
+            won = 1;
+
+        insertion_sort(respuesta[1], 4);
+        insertion_sort(user, 4);
+
+        for (int j = 0; j < merge(respuesta[1], user, 4) && k < 4; j++, k++)
+            hint[k] = '#';
+
+        printw("Hint: %s\n\n", hint);
+
+        refresh();
+    }
+
+    if (!won)
+        printw("Perdiste. La respuesta era: %s\n", respuesta[0]);
+    else
+        printw("Ganaste!\n");
+
+    getch();
+    endwin();
+
+    return 0;
+}
