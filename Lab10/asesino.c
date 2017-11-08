b00ca963fc1cb8ce004a9f1bcf7eeca57d8d7a4c
electrototo <cristobal@liendo.net>
2017-11-08 07:48:31 -0600

    Remove unnecessary variables

--- Lab10/asesino.c
+++ Lab10/asesino.c
@@ -6,26 +6,12 @@
 
 #include <stdio.h>
 
-struct posiciones {
-    int x;
-    int y;
-};
-
-struct cama {
-    int x1;
-    int y1;
-
-    int x2;
-    int y2;
-};
-
 int main() {
     int l, a, n;
 
     scanf("%d %d", &l, &a);
     scanf("%d", &n);
 
-    struct posiciones alacran[n];
     int matriz[l][a];
 
     int x, y;
@@ -35,8 +21,8 @@ int main() {
             matriz[i][j] = 0;
 
     for (int i = 0; i < n; i++) {
-        scanf("%d %d", &alacran[i].x, &alacran[i].y);
-        matriz[alacran[i].y - 1][alacran[i].x - 1] = 1;
+        scanf("%d %d", &x, &y);
+        matriz[y - 1][x - 1] = 1;
     }
 
     int cuenta = 0;
@@ -46,14 +32,11 @@ int main() {
             if (matriz[y][x] != 1) {
                 if (y > 0 && x > 0) {
                     if (matriz[y - 1][x - 1] != 1 && matriz[y - 1][x] != 1 && matriz[y][x - 1] != 1) {
-                        //printf("(%d, %d), ", x, y);
                         cuenta++;
                     }
                 }
             }
         }
-
-        //printf("\n");
     }
 
     printf("%d\n", cuenta);
