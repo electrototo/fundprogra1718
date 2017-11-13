1dd7bda9b9fcc41676065962ea045fe5f643696d
electrototo <cristobal@liendo.net>
2017-11-12 22:41:51 -0600

    agregar comentarios

--- Lab11/battleship.c
+++ Lab11/battleship.c
@@ -8,6 +8,10 @@
 #include <stdlib.h>
 #include <time.h>
 
+// crea la esttructura que va a tener el jugador y la
+// computadora.
+// aqui se guardan las posiciones de los barcos junto 
+// con los respectivos puntajes
 struct jugador {
     char tablero[5][5];
     char tiros[5][5];
@@ -15,62 +19,95 @@ struct jugador {
 
 };
 
+// guardo "struct jugador" comoo "jugador"
 typedef struct jugador jugador;
 
+// crea una estructura para almacenar las coordenadas
+// de las posiciones estilo plano cartesiano para poder
+// guardarlas en un arreglo a futuro y evitar que la
+// computadora tire mas de una vez en el mismo lugar
 struct posicion {
     int x;
     int y;
 };
 
+// guardo "struct posicion" unicamente como "posicion"
 typedef struct posicion posicion;
 
+// estructura conttrol que va a usar la computadora para
+// evitar tirar en la misma posicioon mas de una vez
 struct control_computadora {
     int index;
     posicion posible[25];
 };
 
-
+// funcion que imprime el tablero a partir de un arreglo
+// bidimensional
 void print_tablero(char tablero[5][5]) {
+    // recorre el tablero desde el ultimo elemento hasta
+    // el primero para poder imprimirlo de una manera mas
+    // "humana"
     for (int i = 5; i >= 0; i--) {
 
         for (int j = 0; j < 6; j++) {
+            // imprime el caracter que se esta almacenando
+            // en la posicion x, y
             if (j < 5 && i > 0)
                 printf("%c ", tablero[i - 1][j]);
+            // si es que se encuentra en el extremo del tablero,
+            // imprime el numero de hilera en la que se encuentra
             else if (j == 5 && i > 0)
                 printf("|%d\n", i);
+            // si se encuentra al final del tablero, imprime el
+            // separador para imprimir los numeros de columnas
             else if (i == 0 && j < 6)
                 printf("-%s", (j == 5) ? "\n" : "-");
         }
 
     }
-    
+    // imprime los numeros de columna actual
     for (int i = 0; i < 5; i++)
         printf("%c%s", 'A' + i, (i == 4) ? "\n" : " ");
 }
 
+// funcion auxiliar que sirve para convertir una letra mayuscula a
+// minuscula
 char to_lowercase(char letter) {
+    // si la letra es minuscula, no hay que hacer ningun cambio
     if (letter >= 'a' && letter <= 'z') {
         return letter;
     }
+    // si la letra es mayuscula, regresa el valor ascii para la
+    // lettra correspondiente en minuscula
     else if(letter >= 'A' && letter <= 'Z')
         return letter + ('a' - 'A');
+    // si no es letra, marca error
     else
         return 0;
 }
 
+// funcion auxiliar que convierte un caracter a numero
 char catoi(char letter) {
     return (letter >= '1' && letter <= '5') ? letter - '0' : -1;
 }
 
+// funcion auxiliar que convierte una coordenada como "a1"
+// en coordenadas del plano cartesiano, "x" y "y"
 int getxy(char *coord, int *x, int *y) {
     char c;
     int py;
 
+    // convierte la primera parte de la coordenada en
+    // minuscula
     c = to_lowercase(coord[0]);
+    // y obten el valor numerico de la segunda parte
     py = catoi(coord[1]);
 
+    // si hubo algun error, regresa un valor 0
     if (!c || py == -1 || c > 'e')
         return 0;
+    // de lo contrario, guarda la coordenada en las variables
+    // "x" y "y"
     else {
         *x = c - 'a';
         *y = py - 1;
@@ -79,9 +116,12 @@ int getxy(char *coord, int *x, int *y) {
     }
 }
 
+// funcion que pregunta la orientacion del barco
 int barco_pos(int n_barco) {
     int p;
 
+    // repite hasta que el usuario haya ingresado una opcion
+    // valida
     do {
         printf("Como deseas posicionar el barco %d?\n", n_barco);
         printf("[1] Vertical\n");
@@ -94,6 +134,9 @@ int barco_pos(int n_barco) {
     return p;
 }
 
+// funcion auxiliar para verificar si el barco se encuentra
+// dentro del tablero o si no hay ningun barco previamente
+// posicionado en el rango que se esta especificando
 int check_position(char tablero[5][5], int x, int y, int length, int orientation) {
     // orientation: 1 si es vertical, 2 si es horizontal
     int good = 1;
@@ -103,7 +146,7 @@ int check_position(char tablero[5][5], int x, int y, int length, int orientation
         if ((y < 2 && length == 3) || (y < 1 && length == 2))
             good = 0;
 
-        // checa que no haya algun barco dentro del rang
+        // checa que no haya algun barco dentro del rango
         for (int i = y; i > y - length; i--) {
             if (tablero[i][x] == '|' || tablero[i][x] == '=') {
                 good = 0;
@@ -115,7 +158,7 @@ int check_position(char tablero[5][5], int x, int y, int length, int orientation
         if ((x > 2 && length == 3) || (x > 3 && length == 2))
             good = 0;
 
-        // checa que no haya algun barco dentro del rang
+        // checa que no haya algun barco dentro del rango
         for (int i = x; i < x + length; i++) {
             if (tablero[y][i] == '|' || tablero[y][i] == '=') {
                 good = 0;
@@ -126,9 +169,14 @@ int check_position(char tablero[5][5], int x, int y, int length, int orientation
     return good;
 }
 
+// funcion que recorre todos los valores de un arreglo una unidad
+// a la derecha.
+// sirve para poder evitar que la computadora tire mas de una vez en el mismo lugar
 void recorrer(struct control_computadora *c, int from) {
     posicion tmp;
 
+    // recorre todo el arreglo desde "from" hasta el final una
+    // unidad a la derecha
     c->index--;
     for (int i = from; i < 24; i++) {
         tmp = c->posible[i];
@@ -138,6 +186,7 @@ void recorrer(struct control_computadora *c, int from) {
 }
 
 int main() {
+    // imprime las instrucciones del juego
     printf("Instrucciones:\n");
     printf("El objetivo del juego es hundir todos los barcos enemigos.\n");
     printf("Al principio tendras que posicionar todos los barcos\n");
@@ -153,12 +202,16 @@ int main() {
     printf("El primero en hundir todos los barcos gana.\n\nBuena suerte.\n");
     srand(time(0));
 
+    // establece las variables que se van a utilizar
     jugador jugador1, computadora;
 
     struct control_computadora c_tiros;
+    jugador1.puntaje = 0, computadora.puntaje = 0;
+
     c_tiros.index = 0;
 
-    jugador1.puntaje = 0, computadora.puntaje = 0;
+    // limpia los tableros de cualquier basura que se encuentre dentro
+    // de ellos
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
             jugador1.tablero[i][j] = ' ';
@@ -174,17 +227,21 @@ int main() {
     char coordenadas[2], placeholder;
     int x, y, orientacion, validas = 1, length = 3;
 
-    // Llena de forma aleatoria los barcos de la computadora
+    // posiciona de forma aleatoria los barcos de la computadora
     for (int i = 0; i < 3; i++) {
+        // escoge una orientacion al azar
         orientacion = (rand() % 2) + 1;
         length = (i == 0) ? 3 : 2;
 
+        // dependiendo de la orientacion es como se va a imprimir
+        // el barco
         if (orientacion == 1)
             placeholder = '|';
         else
             placeholder = '=';
 
-        // esto podria ser mejorado
+        // encuentra una posicion aleatoria que no interfiera con
+        // los otros barcos
         do {
             if (orientacion == 1) {
                 x = rand() % 5;
@@ -204,6 +261,7 @@ int main() {
             }
         } while(!check_position(computadora.tablero, x, y, length, orientacion));
 
+        // posiciona los barcos en la posicion correspondiente
         if (orientacion == 1) {
             for (int j = 0; j < length; j++)
                 computadora.tablero[y--][x] = placeholder;
@@ -214,8 +272,9 @@ int main() {
         }
     }
 
-    printf("\nTablero de la computadora:\n");
-    print_tablero(computadora.tablero);
+    // imprime el tablero del jugador
+    printf("\nTablero del juego:\n");
+    print_tablero(jugador1.tablero);
 
     // Posiciona los barcos del jugador
     for (int i = 0; i < 3; i++) {
@@ -229,16 +288,17 @@ int main() {
         else
             placeholder = '=';
 
+        // repite hasta que se ingresen coordenadas correctas
         do {
             validas = 1;
 
             printf("Ingresa las coordenadas: ");
             scanf("%s", coordenadas);
 
+            // verifica que las coordenadas sean correctas
             if (getxy(coordenadas, &x, &y)) {
-                // verificar si entra dentro del rango del tablero
-                printf("Coordenadas: (%d, %d)\n", x, y);
-
+                // verifica que no haya ningun barco en el camino
+                // del otro barco
                 if (check_position(jugador1.tablero, x, y, length, orientacion)) {
                     if (orientacion == 1) {
                         for (int j = 0; j < length; j++)
@@ -255,27 +315,35 @@ int main() {
             else
                 validas = 0;
 
+            // si hay algun error en la validacion, notifica al usuario
             if (!validas)
                 printf("\tCoordenadas no validas\n\n");
 
         } while (!validas);
 
+        // despues de cada posicionamiento del barco, imprime los barcos actuales
         printf("\nTus barcos:\n");
         print_tablero(jugador1.tablero);
     }
 
     printf("\nEl juego ha inciado\n");
     int c_tiro = 0;
+    // se va a ejecutar hasta que alguno de los dos haya ganado
     while (jugador1.puntaje < 7 && computadora.puntaje < 7) {
+        // se vuelve a preguntar las coordenadas hasta que estas esten correctas
         do {
-            // Turno del jugador
+            // el juego ya empezo, pregunta las coordenadas al jugador
             printf("Ingresa las coordenadas: ");
             scanf("%s", coordenadas);
 
             validas = 1;
 
+            // es la misma verificacion que la linea 292
             if (getxy(coordenadas, &x, &y)) {
+                // verifica que no haya tirado en la misma posicion
                 if (jugador1.tiros[y][x] != 'x' && jugador1.tiros[y][x] != 'O') {
+                    // si le atino a un barco enemigo, marcalo en el mapa y
+                    // aumenta su punttaje
                     if (computadora.tablero[y][x] == '|' || computadora.tablero[y][x] == '=') {
                         jugador1.tiros[y][x] = 'x';
                         jugador1.puntaje++;
@@ -295,15 +363,19 @@ int main() {
         } while(!validas);
 
 
-        // turno de la computadora
+        // escoge una posicion al azar de todas las coordenadas que previamente
+        // no se hayan escogido
         c_tiro = rand() % c_tiros.index;
         x = c_tiros.posible[c_tiro].x;
         y = c_tiros.posible[c_tiro].y;
 
+        // si le atina al barco del jugador, marcalo en el mapa del jugador
+        // y aumenta el puntaje de la computadora
         if (jugador1.tablero[y][x] == '|' || jugador1.tablero[y][x] == '=') {
             jugador1.tablero[y][x] = 'x';
             computadora.puntaje++;
         }
+        // de lo contrario solo marca donde tiraron con una O
         else {
             jugador1.tablero[y][x] = 'O';
         }
@@ -319,6 +391,7 @@ int main() {
         printf("\n");
     }
 
+    // imprime los mensajes, dependiendo del ganador
     if (jugador1.puntaje == 7)
         printf("\nFelcidades, derrotaste a la flota enemiga\n");
     else
